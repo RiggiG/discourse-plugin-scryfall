10199645f8c12fb03fcbda8ed8c1a1b0b9a84f6f
RiggiG <riggi.gavin@gmail.com>
2025-10-07 19:53:57 -0500

    try: initial attempts for scryfall link replacement

--- plugin.rb
+++ plugin.rb
@@ -1,21 +1,43 @@
 # frozen_string_literal: true
 
-# name: discourse-plugin-name
-# about: TODO
+# name: discourse-plugin-scryfall
+# about: Converts [[card name]] to Scryfall search links for Onebox embeds
 # meta_topic_id: TODO
 # version: 0.0.1
 # authors: Discourse
 # url: TODO
 # required_version: 2.7.0
 
-enabled_site_setting :plugin_name_enabled
+enabled_site_setting :scryfall_plugin_enabled
 
-module ::MyPluginModule
-  PLUGIN_NAME = "discourse-plugin-name"
+module ::ScryfallPlugin
+  PLUGIN_NAME = "discourse-plugin-scryfall"
 end
 
 require_relative "lib/my_plugin_module/engine"
 
 after_initialize do
-  # Code which should run after Rails has finished booting
+  # Register markdown processor for Scryfall card links
+  on(:before_post_process_cooked) do |doc, post|
+    if SiteSetting.scryfall_plugin_enabled
+      ScryfallPlugin.process_scryfall_links(doc)
+    end
+  end
+end
+
+module ::ScryfallPlugin
+  def self.process_scryfall_links(doc)
+    doc.css('p').each do |paragraph|
+      next if paragraph.content.exclude?('[[')
+      
+      new_html = paragraph.inner_html.gsub(/\[\[([^\]]+)\]\]/) do |match|
+        card_name = $1.strip
+        encoded_name = CGI.escape(card_name)
+        scryfall_url = "https://scryfall.com/search?q=#{encoded_name}&unique=cards&as=grid&order=name"
+        "<a href=\"#{scryfall_url}\">#{card_name}</a>"
+      end
+      
+      paragraph.inner_html = new_html
+    end
+  end
 end
