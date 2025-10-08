af30f9e9de1a50d96f55d43352639ec07b554c10
RiggiG <riggi.gavin@gmail.com>
2025-10-08 17:49:16 -0500

    add: setting for either onebox (default) or custom (not yet implemented) display of cards

--- plugin.rb
+++ plugin.rb
@@ -10,6 +10,8 @@
 
 enabled_site_setting :scryfall_plugin_enabled
 
+register_site_setting_type :enum, ScryfallCardDisplayType
+
 module ::ScryfallPlugin
   PLUGIN_NAME = "discourse-plugin-scryfall"
 end
@@ -38,17 +40,32 @@ module ::ScryfallPlugin
       # Process each card reference
       paragraph.inner_html.gsub!(/\[\[([^\]]+)\]\]/) do |match|
         card_name = $1.strip
-        encoded_name = CGI.escape(card_name)
-        scryfall_url = "https://scryfall.com/search?q=#{encoded_name}&unique=cards&as=grid&order=name"
         
-        # Insert as new paragraph after current one for proper Onebox processing
-        new_p = fragment.document.create_element('p')
-        new_p.inner_html = "<a href=\"#{scryfall_url}\" class=\"onebox\" target=\"_blank\">#{scryfall_url}</a>"
-        paragraph.add_next_sibling(new_p)
-        
-        # Replace original with just the card name as text
-        card_name
+        case SiteSetting.scryfall_card_display_type
+        when 'onebox'
+          create_onebox_link(fragment, paragraph, card_name)
+        when 'custom'
+          # Future: custom card display implementation
+          card_name
+        else
+          card_name
+        end
       end
     end
+  end
+  
+  private
+  
+  def self.create_onebox_link(fragment, paragraph, card_name)
+    encoded_name = CGI.escape(card_name)
+    scryfall_url = "https://scryfall.com/search?q=#{encoded_name}&unique=cards&as=grid&order=name"
+    
+    # Insert as new paragraph after current one for proper Onebox processing
+    new_p = fragment.document.create_element('p')
+    new_p.inner_html = "<a href=\"#{scryfall_url}\" class=\"onebox\" target=\"_blank\">#{scryfall_url}</a>"
+    paragraph.add_next_sibling(new_p)
+    
+    # Replace original with just the card name as text
+    card_name
   end
 end
