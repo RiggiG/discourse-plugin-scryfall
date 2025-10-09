e9d219b10b154ea343085ca8cff7bc3d03266ce1
RiggiG <riggi.gavin@gmail.com>
2025-10-09 01:48:01 -0500

    change: make name match folder from git repo + increment version number

--- plugin.rb
+++ plugin.rb
@@ -1,25 +1,22 @@
 # frozen_string_literal: true
 
-# name: discourse-scryfall
+# name: discourse-plugin-scryfall
 # about: Converts [[card name]] to Scryfall search links for Onebox embeds
 # meta_topic_id: TODO
-# version: 0.0.4
-# authors: Discourse
+# version: 0.0.5
+# authors: RiggiG
 # url: TODO
 # required_version: 2.7.0
 
 enabled_site_setting :scryfall_plugin_enabled
 
 module ::ScryfallPlugin
-  PLUGIN_NAME = "discourse-scryfall"
+  PLUGIN_NAME = "discourse-plugin-scryfall"
 end
 
 require_relative "lib/scryfall_plugin/engine"
 
 after_initialize do
-  # Custom onebox engine is automatically loaded from lib/onebox/
-  # No registration needed
-
   # Process raw markdown before post creation
   on(:before_create_post) do |post_creator|
     if SiteSetting.scryfall_plugin_enabled
