45be47ee433e5a38795550480eaf779cc7441424
RiggiG <riggi.gavin@gmail.com>
2025-10-09 02:03:04 -0500

    add: missing lib autoload in engine

--- lib/scryfall_plugin/engine.rb
+++ lib/scryfall_plugin/engine.rb
@@ -4,5 +4,6 @@ module ::ScryfallPlugin
   class Engine < ::Rails::Engine
     engine_name PLUGIN_NAME
     isolate_namespace ScryfallPlugin
+    config.autoload_paths << File.join(config.root, "lib")
   end
 end
